7fa3aa684331223e6ce6515d629e3461dc44cf19
luchodbr <36521786+luchodbr@users.noreply.github.com>
2018-06-22 15:00:28 -0300

    Add files via upload

---
+++ Pair/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+#include "parser.h"
+/****************************************************
+    Menu:
+        1. Parse del archivo data.csv
+        2. Listar Empleados
+        3. Ordenar por nombre
+        4. Agregar un elemento
+        5. Elimina un elemento
+        6. Listar Empleados (Desde/Hasta)
+*****************************************************/
+
+
+int main()
+{
+    ArrayList* listaEmpleados = al_newArrayList();
+    Employee* auxiliarEmpleado;
+
+    FILE* pFile= fopen("data.csv", "r");
+    parserEmployee(pFile, listaEmpleados);
+
+    auxiliarEmpleado = employee_newParametros("12","Jorge", "Ejemplo", "true");
+    al_add(listaEmpleados, auxiliarEmpleado);
+
+    auxiliarEmpleado = employee_newParametros("-1","Ricardo", "Ejemplo", "true");
+    al_add(listaEmpleados, auxiliarEmpleado);
+
+    al_sort(listaEmpleados, employee_compare, 0);
+
+    int i;
+    for(i=0;i<al_len(listaEmpleados);i++){
+        employee_print(al_get(listaEmpleados, i));
+    }
+
+
+
+    return 0;
+}
---
+++ Pair/parser.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+
+int parserEmployee(FILE* pFile , ArrayList* pArrayListEmployee)
+{
+    int retorno = -1;
+    //tener donde dejar lo que voy a leer (buffers)
+    //buffers gigantes
+    char bId[4096];
+    char bName[4096];
+    char bLastName[4096];
+    char bIsEmpty[4096];
+
+    Employee* auxiliarempleado;
+
+    if(pFile != NULL){
+
+        retorno = 0;
+        //para descartar la primera linea, se hace un fscanf sin agregarla al array list
+        //ni guardarla en un auxEmpleado
+        fscanf(pFile, "%[^,],%[^,],%[^,],%[^\n]\n",bId, bName, bLastName,bIsEmpty);
+        while(!feof(pFile)){
+
+            fscanf(pFile, "%[^,],%[^,],%[^,],%[^\n]\n",
+                   bId, bName, bLastName,bIsEmpty);
+            //el auxiliar no es necesario, podria ir dentro de la funcion al_add
+            auxiliarempleado = employee_newParametros(bId, bName, bLastName, bIsEmpty);
+            al_add(pArrayListEmployee, auxiliarempleado);
+        }
+    }
+    return retorno;
+}
+
+
+
+
